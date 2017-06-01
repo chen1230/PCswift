ac09c18602727943b2b17ab014471a2eb2d829c1
wow_chen <1042528249@qq.com>
2017-06-01 13:58:27 +0800

    提交初始化项目

---
+++ DouYuApp/DouYuApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DouYuApp
+//
+//  Created by chen on 2017/6/1.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
